7125873afeb32c1d64c7fdcbd813f03b392cc047
Dylan Floyd <floyd800@gmail.com>
2021-08-13 15:35:33 -0700

    test

--- app.js
+++ app.js
@@ -15,19 +15,19 @@ let score2 = 0;
   // update DOM to reflect the new state
 
 function zeroifyScore(score) {
-  let string = String(score);
-  for(let i = string.length; i < 3; i++) {
-    string = '0' + string;
-  }
-  return string;
+    let string = String(score);
+    for (let i = string.length; i < 3; i++) {
+        string = '0' + string;
+    }
+    return string;
 }
 
 TDButton1.addEventListener('click', () => {
-  score1 += 7;
-  score1El.textContent = zeroifyScore(score1);
+    score1 += 7;
+    score1El.textContent = zeroifyScore(score1);
 });
 
 TDButton2.addEventListener('click', () => {
-  score2 += 7;
-  score2El.textContent = zeroifyScore(score2);
+    score2 += 7;
+    score2El.textContent = zeroifyScore(score2);
 });
